7b7f1b5fbc735ce1f7a3d49ca9308bd820b8b514
wchen342 <feiyu2817@gmail.com>
2021-06-04 20:34:44 +0300

    Remove build time flag workaround

--- android/java/org/chromium/chrome/browser/firstrun/P3aOnboardingActivity.java
+++ android/java/org/chromium/chrome/browser/firstrun/P3aOnboardingActivity.java
@@ -80,8 +80,8 @@ public class P3aOnboardingActivity extends FirstRunActivityBase {
                     }
                 });
         ImageView p3aOnboardingImg = findViewById(R.id.p3a_onboarding_img);
-        // Same as above
-        if (BraveConfig.P3A_ANDROID_BROWSERTEST)
+        // Remove the condition when https://github.com/brave/brave-browser/issues/16244 is resolved
+        if (!CommandLine.isInitialized())
             p3aOnboardingImg.setImageResource(R.drawable.ic_brave_logo);
         else
             p3aOnboardingImg.setImageResource(isFirstInstall
